389862ebb06d8e99589e83e44fbc7508e2f95c45
Marie-mazaudier <mariemazaudier.dev@gmail.com>
2024-09-25 20:21:37 +0200

    ajout cross-fetch npm

--- app/lib/apolloClient.ts
+++ app/lib/apolloClient.ts
@@ -3,13 +3,12 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import fetch from 'cross-fetch'; // Utilisation de fetch pour les environnements serveur et client
 
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
   cache: new InMemoryCache(),
   link: new HttpLink({
     uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`, // GraphQL endpoint
-    fetch,
+    fetch, // Utilisation de cross-fetch pour les requêtes HTTP
     headers: {
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`, // Utilisation du token
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`, // Utilisation du token API
     },
   }),
 });
