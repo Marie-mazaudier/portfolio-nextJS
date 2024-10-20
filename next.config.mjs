e125c1219f010d9481e2acef0e8f5c94be92b768
Marie-mazaudier <mariemazaudier.dev@gmail.com>
2024-10-20 20:08:16 +0200

    robot.tsxt maj

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import BuilderDevTools from "@builder.io/dev-tools/next";
+import TerserPlugin from "terser-webpack-plugin"; // Importer TerserPlugin
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = BuilderDevTools()({
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.module.rules.push({
       test: /\.svg$/,
       use: [
@@ -22,6 +23,22 @@ const nextConfig = BuilderDevTools()({
 
     // Configurer l'alias @ pour pointer vers le dossier app
     config.resolve.alias["@"] = path.resolve(__dirname, "./app");
+
+    // Ajout de TerserPlugin pour supprimer les console.log et les commentaires
+    if (!isServer) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // Supprime tous les console.log
+            },
+            output: {
+              comments: false, // Supprime les commentaires du code minifié
+            },
+          },
+        }),
+      ];
+    }
 
     return config;
   },
